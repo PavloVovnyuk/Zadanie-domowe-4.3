8e6edad56a39c026b9d43577f099c90e384abfc2
Pavlo <pawel.wowniuk@gmail.com>
2019-01-20 17:39:37 +0100

    Zadanie domowe 4.3

---
+++ src/Triangle.java
@@ -0,0 +1,36 @@
+public class Triangle {
+    private int a;
+    private int b;
+    private int c;
+
+
+    public Triangle() {
+    }
+
+    public int getA() {
+        return a;
+    }
+
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+}
+
